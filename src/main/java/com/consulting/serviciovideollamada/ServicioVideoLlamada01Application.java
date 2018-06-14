2268bc0d3676f5146de03b0aa0b8b20e37a249a0
Jose garcia <newmoon798@gmail.com>
2018-06-14 12:03:58 -0500

    configuracion de para un war

--- src/main/java/com/consulting/serviciovideollamada/ServicioVideoLlamada01Application.java
+++ src/main/java/com/consulting/serviciovideollamada/ServicioVideoLlamada01Application.java
@@ -2,11 +2,21 @@ package com.consulting.serviciovideollamada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServicioVideoLlamada01Application {
+public class ServicioVideoLlamada01Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioVideoLlamada01Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(ServicioVideoLlamada01Application.class);
+	}
+	
+	
 }
